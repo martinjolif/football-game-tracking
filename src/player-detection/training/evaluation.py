7ffff4792db6cb2936d742e9c1a2e6f55fbeb91e
martinjolif <martinjolif@gmail.com>
2025-10-29 11:39:28 +0100

    update: enhance configuration and refactor training and evaluation scripts for YOLO model

--- src/player-detection/training/evaluation.py
+++ src/player-detection/training/evaluation.py
@@ -2,24 +2,24 @@ import os
 
 from ultralytics import YOLO
 
-from config import DATA_ROOT
-
-def evaluate_trained_model(model_path: str) -> None:
+def evaluate_trained_model(model_path: str, data_root: str) -> None:
     """Evaluate a trained YOLO model on a test dataset."""
     # Load the trained YOLO model
     model = YOLO(model_path)
 
     # Evaluate on specific images
-    for filename in os.listdir(DATA_ROOT + "/test/images/"):
+    for filename in os.listdir(data_root + "/test/images/"):
         if filename.endswith(".jpg"):
-            img_path = os.path.join(DATA_ROOT + "/test/images/", filename)
+            img_path = os.path.join(data_root + "/test/images/", filename)
             result = model(img_path)
             result[0].show(line_width=1)
 
     # Evaluate on the test set (must be defined in data.yaml)
-    model.val(data=DATA_ROOT + "/data.yaml", split="test")
+    model.val(data=data_root + "/data.yaml", split="test")
 
 
 if __name__ == "__main__":
-    MODEL_PATH = "runs/train/yolov8-football-yolov8n2/weights/best.pt"  # path to trained model
-    evaluate_trained_model(MODEL_PATH)
+    from config import DATA_ROOT, MODEL_DIR, MODEL_EVAL_DIR
+
+    MODEL_PATH = MODEL_DIR + MODEL_EVAL_DIR  # path to trained model
+    evaluate_trained_model(MODEL_PATH, DATA_ROOT)
--- src/player-detection/training/train.py
+++ src/player-detection/training/train.py
@@ -2,7 +2,15 @@ from pathlib import Path
 
 from ultralytics import YOLO
 
-def train_yolo(data_yaml: str, model_name: str, epochs:int, batch_size: int, imgsz: int, device: str) ->  None:
+def train_yolo(
+        data_yaml: str,
+        model_name: str,
+        model_dir: str,
+        epochs: int,
+        batch_size: int,
+        imgsz: int,
+        device: str
+) ->  None:
     # Load the pretrained YOLO model
     model_checkpoint = f"{model_name}.pt"
     model = YOLO(model_checkpoint)
@@ -13,9 +21,9 @@ def train_yolo(data_yaml: str, model_name: str, epochs:int, batch_size: int, img
         epochs=epochs,
         batch=batch_size,
         imgsz=imgsz,
-        project="runs/train",
+        project=model_dir,
         name=f"football-player-detection-{Path(model_checkpoint).stem}",
-        device=device  # use GPU if available by setting device='0', use 'cpu' for CPU
+        device=device
     )
 
     # Evaluate on the test set (must be defined in data.yaml)
@@ -27,6 +35,6 @@ def train_yolo(data_yaml: str, model_name: str, epochs:int, batch_size: int, img
     print(f"Training completed. Best model saved at {best_model_path}")
 
 if __name__ == "__main__":
-    from config import BATCH_SIZE, EPOCHS, IMG_SIZE, DEVICE, MODEL_NAME, DATA_ROOT
+    from config import TRAIN_BATCH_SIZE, EPOCHS, IMG_SIZE, DEVICE, MODEL_NAME, DATA_ROOT, MODEL_DIR
 
-    train_yolo(DATA_ROOT + "/data.yaml", MODEL_NAME, EPOCHS, BATCH_SIZE, IMG_SIZE, DEVICE)
+    train_yolo(DATA_ROOT + "/data.yaml", MODEL_NAME, MODEL_DIR, EPOCHS, TRAIN_BATCH_SIZE, IMG_SIZE, DEVICE)
