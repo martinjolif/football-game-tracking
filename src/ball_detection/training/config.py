6c02a7b30f50b0bdfd46874ac3ff787f87fcb0b9
martinjolif <martinjolif@gmail.com>
2025-10-29 19:43:26 +0100

    add configuration, training and evaluation scripts for YOLO ball detection

---
+++ src/ball_detection/training/config.py
@@ -0,0 +1,15 @@
+# Global configuration settings for training and evaluating a YOLO object detection model
+IMG_SIZE = 640
+DEVICE = "mps"  # or "cpu", "0" for GPU
+MODEL_NAME = "yolov8n" # Pretrained YOLO model name
+DATA_ROOT = "src/ball_detection/data/yolov8-format" # Root directory for dataset
+MODEL_DIR = "src/ball_detection/models" # Directory to save trained models
+
+# Training configuration parameters for YOLO object tracking model
+TRAIN_BATCH_SIZE = 16
+EPOCHS = 50
+
+# Evaluation configuration
+EVAL_BATCH_SIZE = 16
+MODEL_EVAL_PATH = "football-ball-detection-yolov8n/weights/best.pt" # Path to the trained model for evaluation
+PLOTS = True # Whether to generate plots during evaluation
