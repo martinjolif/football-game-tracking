c7e3dbb329ae6d89cedd7f5ae088268b3b2c591e
martinjolif <martinjolif@gmail.com>
2025-11-11 15:44:34 +0100

    feat: add device selection for model inference based on available hardware

--- src/pitch_detection/api/config.py
+++ src/pitch_detection/api/config.py
@@ -1,3 +1,11 @@
+import torch
+
 INFERENCE_MODEL_PATH = "src/pitch_detection/models/football-pitch-detection-yolov8n-pose7/weights/best.pt"  # Path to the trained model for inference
 ALLOWED_IMG_TYPES = {"image/jpeg", "image/png"}
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5 Mo
+if torch.cuda.is_available():
+    DEVICE = "0"
+elif torch.backends.mps.is_available():
+    DEVICE = "mps"
+else:
+    DEVICE = "cpu"
--- src/player_detection/api/config.py
+++ src/player_detection/api/config.py
@@ -1,3 +1,11 @@
+import torch
+
 INFERENCE_MODEL_PATH = "src/player_detection/models/football-player-detection-yolov8n/weights/best.pt"  # Path to the trained model for inference
 ALLOWED_IMG_TYPES = {"image/jpeg", "image/png"}
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5 Mo
+if torch.cuda.is_available():
+    DEVICE = "0"
+elif torch.backends.mps.is_available():
+    DEVICE = "mps"
+else:
+    DEVICE = "cpu"
