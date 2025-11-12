fc621069aebb59ee90234ed10916cba9e1a1f5a7
martinjolif <martinjolif@gmail.com>
2025-11-12 14:42:43 +0100

    feat: enable pitch radar visualization in video frames

--- src/app/video_to_frames.py
+++ src/app/video_to_frames.py
@@ -5,11 +5,13 @@ import cv2
 import supervision as sv
 
 from src.app.image_api import call_image_apis
-from src.app.player_tracking import detections_from_results
+from src.app.api_to_supervision import detections_from_results, keypoints_from_pose_results
 from src.app.player_tracking import visualize_frame
+from src.app.pitch_radar_visualization import render_pitch_radar
 
 PROCESSED_FRAME_INTERVAL = 50
-PLAYER_TRACKING_VIZ = True
+PLAYER_TRACKING_VIZ = False
+PITCH_RADAR_VIZ = True
 
 # Path to your video (it could be an RTSP/HTTP URL or a local file path)
 video_path = "../../england_epl/2014-2015/2015-02-21 - 18-00 Chelsea 1 - 1 Burnley/1_720p.mkv"
@@ -56,7 +58,35 @@ try:
                 tracker=tracker,
                 show_trace=False)
             cv2.imshow("Tracked Players", annotated_frame)
-        cv2.imshow("Simulated Live Stream", frame)
+
+        if PITCH_RADAR_VIZ:
+            player_detection = detections_from_results(
+                results["http://localhost:8000/player-detection/image"]["detections"]
+            )
+            ball_detection = detections_from_results(
+                results["http://localhost:8001/ball-detection/image"]["detections"]
+            )
+            pitch_detection = keypoints_from_pose_results(
+                results["http://localhost:8002/pitch-detection/image"]
+            )
+            h, w, _ = frame.shape
+            annotated_frame = frame.copy()
+            radar = render_pitch_radar(
+                player_detection,
+                ball_detection,
+                pitch_detection,
+            )
+            radar = sv.resize_image(radar, (w//2, h//2))
+            radar_h, radar_w, _ = radar.shape
+            rect = sv.Rect(
+                x=w//2 - radar_w//2,
+                y=h - radar_h,
+                width=radar_w,
+                height=radar_h,
+            )
+            annotated_frame = sv.draw_image(annotated_frame, radar, opacity=0.5, rect=rect)
+
+        cv2.imshow("Simulated Live Stream", annotated_frame)
 
         #if frame_count % PROCESSED_FRAME_INTERVAL == 0:
         #    cv2.imshow("Processed", frame)
