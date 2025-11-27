69c2ae406a48a61f7c66d969a15a4b1bbeeb0f8e
martinjolif <martinjolif@gmail.com>
2025-11-27 17:44:16 +0100

    move some files in a team clustering folder

--- tests/app/test_crop_players.py
+++ tests/app/test_crop_players.py
@@ -1,4 +1,4 @@
-from app.crop_players import yolo_to_bbox
+from app.team_clustering.crop_players import yolo_to_bbox
 
 def test_yolo_to_bbox():
     assert yolo_to_bbox(xc=1/2, yc=1/2, w=1/2, h=1/2, img_w=640, img_h=380, padding=0) == (160, 95, 480, 285)
